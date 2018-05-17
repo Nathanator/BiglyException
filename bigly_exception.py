aa077ff7e22441aea3a21d548afda119db365ce0
Matt Wheeler <m@funkyhat.org>
2018-05-17 21:14:21 +0100

    very sad

--- bigly_exception.py
+++ bigly_exception.py
@@ -1,12 +1,25 @@
 
+import csv
+import re
 import sys
+
+
+with open('trumperror.csv') as f:
+    errors = list(csv.reader(f))
+
+print(errors)
 
 
 def trace_dispatch(frame, event, arg):
     if event == 'exception':
         exc_type, exc, traceback = arg
 
-        exc.args = exc.args + ('very sad',)
+        print('exc', exc)
+
+        for line in errors:
+            if re.search(line[0], str(exc)):
+
+                exc.args = exc.args + (line[1],)
         return trace_dispatch
 
     else:
